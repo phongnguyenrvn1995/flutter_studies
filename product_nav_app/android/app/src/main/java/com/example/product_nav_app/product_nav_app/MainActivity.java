6e51ec16717f80214680bc452cc517dbcf479da2
phongnguyenrvn1995 <61311043+phongnguyenrvn1995@users.noreply.github.com>
2021-07-07 09:02:31 +0700

    init

---
+++ product_nav_app/android/app/src/main/java/com/example/product_nav_app/product_nav_app/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.product_nav_app.product_nav_app;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ product_nav_app/lib/RatingBox.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+
+class RatingBox extends StatefulWidget {
+  @override
+  _RatingBoxState createState() {
+    return _RatingBoxState();
+  }
+}
+
+class _RatingBoxState extends State<RatingBox> {
+  int _rating = 0;
+
+  void _setRatingAs1() {
+    setState(() {
+      _rating = 1;
+    });
+  }
+
+  void _setRatingAs2() {
+    setState(() {
+      _rating = 2;
+    });
+  }
+
+  void _setRatingAs3() {
+    setState(() {
+      _rating = 3;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    double _size = 20;
+    print('rating = ' + _rating.toString());
+    return Row(
+      children: [
+        Container(
+          padding: EdgeInsets.all(0),
+          child: IconButton(
+            icon: _rating >= 1 ?
+            Icon(Icons.star, size: _size,) :
+            Icon(Icons.star_border, size: _size,),
+            onPressed: () { _setRatingAs1(); },
+            color: Colors.red,
+            iconSize: _size,
+          ),
+        ),
+        Container(
+          padding: EdgeInsets.all(0),
+          child: IconButton(
+            icon: _rating >= 2 ?
+            Icon(Icons.star, size: _size,) :
+            Icon(Icons.star_border, size: _size,),
+            onPressed: () { _setRatingAs2(); },
+            color: Colors.green,
+            iconSize: _size,
+          ),
+        ),
+        Container(
+          padding: EdgeInsets.all(0),
+          child: IconButton(
+            icon: _rating >= 3 ?
+            Icon(Icons.star, size: _size,) :
+            Icon(Icons.star_border, size: _size,),
+            onPressed: () { _setRatingAs3(); },
+            color: Colors.blue,
+            iconSize: _size,
+          ),
+        ),
+      ],
+    );
+  }
+}
